a838e3acfbd95fbc33d2553c0603aeafd1036de8
Dwi Kumara Widyatna <dwi@dinamika.ac.id>
2022-10-12 08:16:45 +0700

    add handler in mainactivity

--- app/src/main/java/com/example/test_recruitment/MainActivity.kt
+++ app/src/main/java/com/example/test_recruitment/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.test_recruitment
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            startActivity(Intent(this@MainActivity, login::class.java))
+            finish()
+        }, 3000)
+
     }
 }
